a5bb5cf12a047cf6524d8cd6a1b7901936c1a90e
maurice <maurice>
2007-06-25 17:21:34 +0000

    GRAILSPLUGINS-60: refactored simple pattern code into new utils class
    git-svn-id: https://svn.codehaus.org/grails-plugins/grails-searchable/trunk@758 832c1c66-4827-0410-8465-ccb17913cd09

---
+++ src/java/org/codehaus/groovy/grails/plugins/searchable/util/PatternUtils.java
@@ -0,0 +1,56 @@
+/*
+ * Copyright 2007 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.codehaus.groovy.grails.plugins.searchable.util;
+
+import java.util.regex.Pattern;
+
+/**
+ * @author Maurice Nicholson
+ */
+public class PatternUtils {
+
+    /**
+     * Make a Pattern from the given string
+     * @param string a String containing wildcards (* and/or ?)
+     * @return a Pattern
+     */
+    public static Pattern makePatternFromWilcardString(String string) {
+        StringBuffer buf = new StringBuffer();
+        for (int i = 0; i < string.length(); i++) {
+            char c = string.charAt(i);
+            switch (c) {
+                case '?':
+                    buf.append(".");
+                    break;
+                case '*':
+                    buf.append(".*");
+                    break;
+                default:
+                    buf.append(c);
+            }
+        }
+        return Pattern.compile(buf.toString());
+    }
+
+    /**
+     * Does the given String contain wildcards (* or ?)
+     * @param string a String
+     * @return true if it contains wildcards
+     */
+    public static boolean hasWildcards(String string) {
+        return string.indexOf('*') > -1 || string.indexOf('?') > -1;
+    }
+}
