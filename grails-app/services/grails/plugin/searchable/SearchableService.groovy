0b54d390eea44ceed6533d626965e46bcb9eaf8a
Peter Ledbrook <peter@cacoethes.co.uk>
2011-07-18 16:31:03 +0100

    Implemented GPSEARCHABLE-206: Moved controller and service into a package.

--- grails-app/services/grails/plugin/searchable/SearchableService.groovy
+++ grails-app/services/grails/plugin/searchable/SearchableService.groovy
@@ -13,6 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+package grails.plugin.searchable
 
 /**
  * Search and index using compass
