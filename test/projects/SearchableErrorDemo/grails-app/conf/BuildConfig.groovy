e6aa1e3375986759113ffdf2bc35218ed32c617f
Peter Ledbrook <peter@cacoethes.co.uk>
2011-07-19 15:21:59 +0100

    Updated some of the test projects to use HSQLDB again. Also reverted dbCreate for SearchableIndexCascading because that seemed to be causing test failures.

--- test/projects/SearchableErrorDemo/grails-app/conf/BuildConfig.groovy
+++ test/projects/SearchableErrorDemo/grails-app/conf/BuildConfig.groovy
@@ -20,7 +20,7 @@ grails.project.dependency.resolution = {
     }
     dependencies {
         // specify dependencies here under either 'build', 'compile', 'runtime', 'test' or 'provided' scopes eg.
-        // runtime 'com.mysql:mysql-connector-java:5.1.5'
+        runtime 'hsqldb:hsqldb:1.8.0.5'
     }
 
 }
--- test/projects/SearchableErrorDemo/grails-app/conf/DataSource.groovy
+++ test/projects/SearchableErrorDemo/grails-app/conf/DataSource.groovy
@@ -1,6 +1,6 @@
 dataSource {
     pooled = true
-    driverClassName = "org.h2.Driver"
+    driverClassName = "org.hsqldb.jdbcDriver"
     username = "sa"
     password = ""
 }
@@ -14,13 +14,13 @@ environments {
     development {
         dataSource {
             dbCreate = "create-drop" // one of 'create', 'create-drop','update'
-            url = "jdbc:h2:mem:devDb"
+            url = "jdbc:hsqldb:mem:devDb"
         }
     }
     test {
         dataSource {
             dbCreate = "update"
-            url = "jdbc:h2:mem:testDb"
+            url = "jdbc:hsqldb:mem:testDb"
         }
     }
     production {
