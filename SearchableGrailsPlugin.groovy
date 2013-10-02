1f274ef96f202dea202fb1ac0d3c9f6553b37aa2
Bobby Warner <bobbywarner@gmail.com>
2013-10-02 13:18:07 -0500

    Releasing version 0.6.5

--- SearchableGrailsPlugin.groovy
+++ SearchableGrailsPlugin.groovy
@@ -35,7 +35,7 @@ import org.springframework.core.JdkVersion
 class SearchableGrailsPlugin {
     static LOG = LogFactory.getLog("grails.plugin.searchable.SearchableGrailsPlugin")
 
-    def version = "0.6.5-SNAPSHOT"
+    def version = "0.6.5"
     def title = 'Searchable Plugin'
     def description = '''
 Adds rich search functionality to Grails domain models.
